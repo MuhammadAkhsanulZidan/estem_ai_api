0f91ef012738e8d91489890b4b242c17334e49de
Ubuntu <rspadjkt@estemai.neo.internal>
2026-07-24 04:09:10 +0700

    up

--- api/generate_controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "This script has been moved back to the scripts directory: api/scripts/generate_controller.php\n";
-echo "You can now run it from the root directory using the runner:\n";
-echo "php run.php generate admin_protocols\n";
-exit(1);
