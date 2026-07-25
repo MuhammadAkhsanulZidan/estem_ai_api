6059f2d51b67dbfcb5eab43a2aec09926b0bf092
Ubuntu <rspadjkt@estemai.neo.internal>
2026-07-26 06:14:16 +0700

    up

--- sql/tables/affiliator_supervision_documents.sql
+++ sql/tables/affiliator_supervision_documents.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS affiliator_supervision_documents (
+DROP TABLE IF EXISTS affiliator_supervision_documents;
+
+CREATE TABLE affiliator_supervision_documents (
     id SERIAL PRIMARY KEY,
     supervision_id INT NOT NULL REFERENCES affiliator_supervisions(id) ON DELETE CASCADE,
-    document_key VARCHAR(100) NOT NULL,
     document_path VARCHAR(250) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
