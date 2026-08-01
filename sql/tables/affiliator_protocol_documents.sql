26e5aad26f4e1052bd0334aefba410fe20cabb2a
Ubuntu <rspadjkt@estemai.neo.internal>
2026-08-01 19:36:43 +0700

    up

--- sql/tables/affiliator_protocol_documents.sql
+++ sql/tables/affiliator_protocol_documents.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS affiliator_protocol_documents;
 CREATE TABLE affiliator_protocol_documents (
     id BIGSERIAL PRIMARY KEY,
     protocol_id BIGINT NOT NULL,
-    document_path   VARCHAR(100)
+    document_path   VARCHAR(255)
 );
